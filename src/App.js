273d1c13fd8fdfb0aecba004ca924a9110c49f2c
olawale <jao.olaniyi@gmail.com>
2023-01-19 00:30:37 +0100

    setup enivronment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import React from "react";
+import Dico from "./components/Dico";
 function App() {
   return (
     <div className="App">
-     
+      <h1>POCKET DICTIONARY</h1>
+      <Dico />
     </div>
   );
 }
---
+++ src/components/Dico.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Dico.css";
+const dico = () => {
+  return <div>dico</div>;
+};
+
+export default dico;
